f3cdd4a5d13269682f025d4ee825550ed774bdce
crazzzytosser <114213358+crazzzytosser@users.noreply.github.com>
2024-07-11 15:00:21 +0300

    Delete HomeWork2 directory

--- HomeWork2/src/main/java/org/example/SportCar.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class SportCar implements Interface {
-    public void draw(String color) {
-        System.out.println("SportCar color: " + color);
-    }
-}
--- HomeWork2/src/main/java/org/example/UnknownCar.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class UnknownCar implements Interface {
-    public void draw(String color) {
-        System.out.println("UnknownCar color: " + color);
-    }
-}
