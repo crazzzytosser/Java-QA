49da036ee75ad4662e25619d7a0126c701d0baac
crazzzytosser <114213358+crazzzytosser@users.noreply.github.com>
2024-07-11 15:00:35 +0300

    Delete src directory

--- src/main/java/HW1/VendingMachine.java
+++
@@ -1,9 +0,0 @@
-package HW1;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface VendingMachine {
-
-    Product getProduct(String name);
-}
--- src/main/java/HW3/Main.java
+++
@@ -1,7 +0,0 @@
-package HW3;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
